c978b290b238ea1bed13150dce83432ae72ee3ed
pamitapn@hotmail.com <pamitapn@hotmail.com>
2025-04-17 00:54:51 +0000

    Add quantidad de transacoes

--- 2_qtd_transacao_12_Idade_na_base.sql
+++ 2_qtd_transacao_12_Idade_na_base.sql
@@ -1,7 +1,8 @@
 WITH tb_clientes_base AS (
     SELECT 
         idCliente,
-        min(dtCriacao) AS dtCriacao_min
+        min(dtCriacao) AS dtCriacao_min,
+        count(*) AS qtd_transacoes
     FROM silver.points.transacoes
     WHERE dtCriacao < '{date}'
     GROUP BY idCliente
@@ -10,5 +11,6 @@ WITH tb_clientes_base AS (
 SELECT 
     '{date}' AS dtSafra,
     idCliente,
-    datediff(date('{date}'), date(dtCriacao_min)) AS qtd_idade_em_dias
+    datediff(date('{date}'), date(dtCriacao_min)) AS qtd_idade_em_dias,
+    qtd_transacoes
 FROM tb_clientes_base
