5a06b399d9a78b8cffe62861cb90ac241295b6c8
teocalvo <teocalvo>
2025-05-27 22:35:07 +0000

    Mlflow

--- train.py
+++ train.py
@@ -81,67 +81,70 @@ imputer_one = imputation.ArbitraryNumberImputer(variables=["vlIFRBruto",
 # COMMAND ----------
 
 # DBTITLE 1,MODEL
-model = ensemble.AdaBoostClassifier(random_state=42)
+model = ensemble.RandomForestClassifier(random_state=42, n_jobs=1)
 
 params = {
-    "n_estimators": [200,500],
-    "learning_rate": [0.5, 0.7, 0.8, 0.85, 0.9],
+    "n_estimators": [100,200,350,500],
+    "min_samples_leaf":[10,50,100]
 }
 
 grid = model_selection.GridSearchCV(estimator=model,
                                     param_grid=params,
                                     cv=3,
                                     scoring='roc_auc',
-                                    n_jobs=2,
-                                    verbose=4)
+                                    n_jobs=2,)
+
 
 model_pipeline = pipeline.Pipeline(steps=[('imputer_max', imputer_tail),
                                           ('imputer_one', imputer_one),
                                           ('classifier', grid)])
 
-model_pipeline.fit(X_train, y_train)
+# Step 1: Importa o MLFlow
+import mlflow
 
-# COMMAND ----------
+# Step 2: Define o experimento que o mlflow vai usar
+mlflow.set_experiment(experiment_id=1481226312561602)
 
-y_pred = model_pipeline.predict(X_train)
-y_proba = model_pipeline.predict_proba(X_train)[:,1]
+# Step 3: Iniciar a Run
+with mlflow.start_run():
 
-acc_treino = metrics.accuracy_score(y_train, y_pred)
-print("taxa de Acurácia em Treino", acc_treino)
+    # Step 4: Habilitar autolog do scikit-learn
+    mlflow.sklearn.autolog()
 
-auc_treino = metrics.roc_auc_score(y_train, y_proba)
-print("taxa de AUC em Treino", auc_treino)
+    model_pipeline.fit(X_train, y_train)
 
-# COMMAND ----------
+    y_pred = model_pipeline.predict(X_train)
+    y_proba = model_pipeline.predict_proba(X_train)[:,1]
 
-pred_test = model_pipeline.predict(X_test)
-prob_test = model_pipeline.predict_proba(X_test)[:,1]
+    acc_treino = metrics.accuracy_score(y_train, y_pred)
+    auc_treino = metrics.roc_auc_score(y_train, y_proba)
 
-acc_test = metrics.accuracy_score(y_test, pred_test)
-print("taxa de Acurácia em test", acc_test)
+    pred_test = model_pipeline.predict(X_test)
+    prob_test = model_pipeline.predict_proba(X_test)[:,1]
 
-acc_bal_test = metrics.balanced_accuracy_score(y_test, pred_test)
-print("taxa de Acurácia Balanceada em test", acc_bal_test)
+    acc_test = metrics.accuracy_score(y_test, pred_test)
+    acc_bal_test = metrics.balanced_accuracy_score(y_test, pred_test)
+    auc_test = metrics.roc_auc_score(y_test, prob_test)
 
-auc_test = metrics.roc_auc_score(y_test, prob_test)
-print("taxa de AUC em test", auc_test)
+    pred_oot = model_pipeline.predict(df_oot[features])
+    prob_oot = model_pipeline.predict_proba(df_oot[features])[:,1]
 
-# Luiz:   0.806154
-# Plinio: 0.806154
+    acc_oot = metrics.accuracy_score(df_oot["flagAtividade"], pred_oot)
+    acc_bal_oot = metrics.balanced_accuracy_score(df_oot["flagAtividade"], pred_oot)
+    auc_oot = metrics.roc_auc_score(df_oot["flagAtividade"], prob_oot)
 
-# COMMAND ----------
-
-pred_oot = model_pipeline.predict(df_oot[features])
-prob_oot = model_pipeline.predict_proba(df_oot[features])[:,1]
-
-acc_oot = metrics.accuracy_score(df_oot["flagAtividade"], pred_oot)
-print("taxa de Acurácia em oot", acc_oot)
-
-acc_bal_oot = metrics.balanced_accuracy_score(df_oot["flagAtividade"], pred_oot)
-print("taxa de Acurácia Balanceada em oot", acc_bal_oot)
-
-auc_oot = metrics.roc_auc_score(df_oot["flagAtividade"], prob_oot)
-print("taxa de AUC em oot", auc_oot)
+    # Step 5: registrar / loggar as métricas
+    metricas = {
+        "acc_treino":acc_treino,
+        "auc_treino":auc_treino,
+        "acc_test":acc_test,
+        "acc_bal_test":acc_bal_test,
+        "auc_test":auc_test,
+        "acc_oot":acc_oot,
+        "acc_bal_oot":acc_bal_oot,
+        "auc_oot":auc_oot,
+    }
+    mlflow.log_metrics(metricas)
 
 # COMMAND ----------
 
